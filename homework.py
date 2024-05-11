0ecc7f38a9a5dcde9a719f9f9304e02baf6abe40
Veronika2102 <167021693+Veronika2102@users.noreply.github.com>
2024-05-11 19:29:41 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,22 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = sum([5, 3, 3, 5, 4]) / len([5, 3, 3, 5, 4])
+print(students)
+students = sum([2, 2, 2, 3]) / len([2, 2, 2, 3])
+print(students)
+students = sum([4, 5, 5, 2]) / len([4, 5, 5, 2])
+print(students)
+students = sum([4, 4, 3]) / len([4, 4, 3])
+print(students)
+students = sum([5, 5, 5, 4, 5]) / len([5, 5, 5, 4, 5])
+print(students)
+grades_ = [[4.0], [2.25], [4.0], [3.6666666666666665], [4.8]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = list(students)
+print(students)
+students.sort(key=str.lower)
+print(students)
+students_dictionary = dict(zip(students, grades_))
+print(students_dictionary)
+
+
+
